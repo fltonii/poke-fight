3993c504a11b07aa2ab6d599479c7ae6c32cc5b5
Lucas Rossi <lucas@sbsistemas.local>
2019-05-03 17:42:49 -0300

    funcao de batalha adicionada

--- src/App.js
+++ src/App.js
@@ -1,44 +1,6 @@
 import React from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider, Query } from "react-apollo";
-import gql from "graphql-tag";
-
-const random = Math.floor(Math.random() * 151);
-
-const client = new ApolloClient({
-  uri: "https://graphql-pokemon.now.sh/?"
-});
-
-const PokeQuery = () => (
-  <Query
-    query={gql`
-      {
-        pokemons(first: ${random}) {
-          name
-          image
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Erro{console.log(error)}</p>;
-
-      const poke = data.pokemons[random - 1];
-
-      return (
-        <div style={{ textAlign: "center" }}>
-          <p style={{ fontSize: 30, fontFamily: "arial" }}>{poke.name}</p>
-          <img src={poke.image} alt={poke.name} style={{ marginTop: 20 }} />
-          <br />
-          <form>
-            <button style={{ marginTop: 50 }}>Outro</button>
-          </form>
-        </div>
-      );
-    }}
-  </Query>
-);
+import { ApolloProvider } from "react-apollo";
+import { client, PokeQuery } from './api';
 
 const App = () => {
   return (
